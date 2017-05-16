49d0fc3330494f492247e7103df24b1f83b5ecd8
grinature <grinature@users.noreply.github.com>
2017-05-16 16:32:25 +0300

    a version accounting the suggested JSHint options
    an enhanced version taking into account the suggested JSHint's enforcing options.

---
+++ js/logger-02-jshinted.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/* exported oLogger*/
+
+// debugger;
+
+var oLogger = {
+    isSysLog : true, // TRUE, by default
+    sysLogPrefix : 'SysLog ',
+    logLevel : 'INFO',  // INFO (* default), SYSTEM, ERROR
+
+    log : function(message) {
+        if(this.isSysLog) {
+            console.log(this.sysLogPrefix + '{' + this.logLevel + '} :: ' + message);
+            return true;
+        }
+        return false;
+    },
+
+    logOff : function () {
+        return (this.isSysLog = false);
+    },
+    logOn : function () {
+        return (this.isSysLog = true);
+    }
+};
