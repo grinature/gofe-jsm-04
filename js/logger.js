d75f55f9d88df9bd5bcf35bd22972223c428f92f
grinature <grinature@users.noreply.github.com>
2017-05-22 16:07:15 +0300

    a tiny update

--- js/logger.js
+++ js/logger.js
@@ -5,7 +5,7 @@
 
 
 // debugger;
-
+/*
 let oLogger = {
     isSysLog : true, // TRUE, by default
     sysLogPrefix : 'SysLog ',
@@ -26,3 +26,30 @@ let oLogger = {
         return (this.isSysLog = true);
     }
 };
+*/
+
+// It is like a module pattern
+let oLogger = ( function() {
+    let isSysLog = true, // TRUE, by default
+        sysLogPrefix = 'SysLog ',
+        logLevel = 'INFO';  // INFO (* default), SYSTEM, ERROR
+
+    return {
+
+        log : function(message) {
+            if(isSysLog) {
+                console.log(sysLogPrefix + '{' + logLevel + '} :: ' + message);
+                return true;
+            }
+            return false;
+        },
+
+        logOff : function () {
+            return ( isSysLog = false );
+        },
+        logOn : function () {
+            return ( isSysLog = true );
+        }
+
+    };
+})();
